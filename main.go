518c619c82bfee6d58894574cafd4943e3ee4d22
alexduzi <duzihd@gmail.com>
2025-07-21 21:29:29 -0300

    adding custom logging + db init config

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/alexduzi/job-openings/router"
+import (
+	"github.com/alexduzi/job-openings/config"
+	"github.com/alexduzi/job-openings/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		panic(err)
+	}
+
 	router.Initialize()
 }
