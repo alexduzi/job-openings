6bc46aa7bca23944be8292a3679e78a8c56dd9f9
alexduzi <duzihd@gmail.com>
2025-07-21 22:03:27 -0300

    sqlite config

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -10,10 +12,22 @@ var (
 )
 
 func Init() error {
+	var err error
+
+	db, err = InitializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("error initializing sqlite: %v", err)
+	}
+
 	return nil
 }
 
 func GetLogger(p string) *Logger {
 	logger = NewLogger(p)
 	return logger
+}
+
+func GetDB() *gorm.DB {
+	return db
 }
