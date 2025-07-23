6b4a8604fea57a6ddc61356b40b23f1adb021b9a
alexduzi <duzihd@gmail.com>
2025-07-23 20:43:32 -0300

    get by id and get list of openings

--- handler/getOpening.go
+++ handler/getOpening.go
@@ -1,13 +1,30 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/alexduzi/job-openings/model"
 	"github.com/gin-gonic/gin"
 )
 
 func GetOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET opening",
-	})
+	id := ctx.Param("id")
+	if id != "" {
+		opening := model.Opening{}
+		if err := db.First(&opening, id).Error; err != nil {
+			sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+			return
+		}
+		sendSuccess(ctx, "get-opening", opening)
+		return
+	}
+
+	openings := []model.Opening{}
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("openings with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "get-openings", openings)
 }
