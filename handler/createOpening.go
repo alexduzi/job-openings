a0098f6422906c8b1de8ec3f2dd2871537b7616a
alexduzi <duzihd@gmail.com>
2025-07-23 19:39:19 -0300

    feature opening create

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -3,11 +3,37 @@ package handler
 import (
 	"net/http"
 
+	"github.com/alexduzi/job-openings/model"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST opening",
-	})
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("error when creating opening: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	logger.Infof("body %+v", request)
+
+	opening := model.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error when creating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opening")
+		return
+	}
+
+	sendSuccess(ctx, "create-opening", opening)
 }
