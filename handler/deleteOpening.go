55940fd9ca937f815e56c6de8b3884a809cbb02e
alexduzi <duzihd@gmail.com>
2025-07-23 20:32:28 -0300

    feature delete opening

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,13 +1,31 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/alexduzi/job-openings/model"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "DELETE opening",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := model.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error deleting opening with id: %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "delete-opening", opening)
 }
--- router/routes.go
+++ router/routes.go
@@ -11,12 +11,12 @@ func initializeRoutes(router *gin.Engine) {
 	{
 		v1.GET("/opening", handler.GetOpeningHandler)
 
-		v1.GET("/opening/{id}", handler.GetOpeningHandler)
+		v1.GET("/opening/:id", handler.GetOpeningHandler)
 
 		v1.POST("/opening", handler.CreateOpeningHandler)
 
-		v1.PUT("/opening/{id}", handler.UpdateOpeningHandler)
+		v1.PUT("/opening/:id", handler.UpdateOpeningHandler)
 
-		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.DELETE("/opening/:id", handler.DeleteOpeningHandler)
 	}
 }
